122e0f42e95d2ec911795f519ecc4553fc573495
Parrot Live user <user@localhost.localdomain>
2025-10-26 05:51:49 +0000

    Added log parsing module

--- main.c
+++ main.c
@@ -6,5 +6,18 @@ int main()
 	b=6;
 	sum=a+b;
 	printf("Sum =%d",sum);
+	int diff;
+	diff=a-b;
+	printf("%d",diff);#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	a=5;
+	b=6;
+	sum=a+b;
+	printf("Sum =%d",sum);
+	return 0;
+}
+
 	return 0;
 }
